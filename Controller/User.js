87b712ef824e799f312957d1022b8ecb488fa89a
Kugan Priyan <88609808+kugan7370@users.noreply.github.com>
2022-08-06 22:53:32 +0530

    Login

--- Controller/User.js
+++ Controller/User.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 import User from '../Model/User.js'
 import { createError } from '../utils/error.js'
-
+import jwt from 'jsonwebtoken'
 
 export const SignUp = async (req, res, next) => {
     try {
@@ -26,4 +26,25 @@ export const SignUp = async (req, res, next) => {
     catch (error) {
         next(error)
     }
+}
+
+export const login = async (req, res, next) => {
+
+    const getUser = await User.findOne({ email: req.body.email })
+
+    if (!getUser)
+        return res.status(400).json({ message: 'invalid email' });
+
+    const isPasswordCorrect = await bcrypt.compare(req.body.Password, getUser.Password)
+
+    if (!isPasswordCorrect)
+        return next(createError(404, "Invalid Password "))
+
+    const token = jwt.sign({ _id: getUser._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+    const { Password, ...otherDetails } = getUser._doc;
+    res.cookie("access_token", token, {
+        httpOnly: true,
+    }).status(200).json({ token, user: otherDetails })
+
 }
--- Routes/User.js
+++ Routes/User.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { SignUp } from '../Controller/User.js'
+import { login, SignUp } from '../Controller/User.js'
 import { isRequestValidated, validateSignupRequest } from '../middleware/Validator.js';
 const router = express.Router()
 
 
 //Auth-Routes
 router.post('/signup', validateSignupRequest, isRequestValidated, SignUp)
-
+router.post('/login', login)
 
 
 
